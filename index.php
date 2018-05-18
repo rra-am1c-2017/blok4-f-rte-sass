adcc87abadab3c6d1758a420d5f0cc6320647723
Arjan de Ruijter <rra.am1a.2017@gmail.com>
2018-05-18 14:02:51 +0200

    einde les

--- index.php
+++ index.php
@@ -13,9 +13,72 @@
     <title>Hello, world!</title>
   </head>
   <body>
-    <h1>Hello, world!</h1>
-
-    <div id="test"></div>
+    <div class="container-fluid">Hier komt de navbar</div>
+    <div class="container">
+      <article class="row">
+        <section class="col-12 col-md-6 col-lg-4 col-xl-20">
+          <div class="card text-center">
+            <div class="card-body">
+              <img src="./images/blauw.png" alt="blauw">
+            </div>
+            <div class="card-footer text-muted">
+              Dit is mooi vierkant
+            </div>
+          </div>
+        </section>
+        <section class="col-6 col-md-6 col-lg-4 col-xl-20">
+          <div class="card text-center">
+            <div class="card-body">
+              <img src="./images/bruin.png" alt="bruin">
+            </div>
+            <div class="card-footer text-muted">
+              Dit is mooi vierkant
+            </div>
+          </div>
+        </section>
+        <section class="col-6 col-md-12 col-lg-4 col-xl-20">
+          <div class="card text-center">
+            <div class="card-body">
+              <img src="./images/geel.png" alt="geel">
+            </div>
+            <div class="card-footer text-muted">
+              Dit is mooi vierkant
+            </div>
+          </div>
+        </section>
+        <section class="col-12 col-md-6 col-lg-4 col-xl-20">
+          <div class="card text-center">
+            <div class="card-body">
+              <img src="./images/goud.png" alt="goud">
+            </div>
+            <div class="card-footer text-muted">
+              Dit is mooi vierkant
+            </div>
+          </div>
+        </section>
+        <section class="col-6 col-md-6 col-lg-4 col-xl-20">
+          <div class="card text-center">
+            <div class="card-body">
+              <img src="./images/grijs.png" alt="grijs">
+            </div>
+            <div class="card-footer text-muted">
+              Dit is mooi vierkant
+            </div>
+          </div>
+        </section>
+        <section class="col-6 col-md-12 col-lg-4 col-xl-20">
+          <div class="card text-center">
+            <div class="card-body">
+              <img src="./images/groen.png" alt="groen">
+            </div>
+            <div class="card-footer text-muted">
+              Dit is mooi vierkant
+            </div>
+          </div>
+        </section>
+      </article>
+    </div>
+    
 
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
